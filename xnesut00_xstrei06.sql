7f6c2245a9c279763c70a9c8fb6a9cb5117b8306
Jaroslav Streit <Jarek.Streit@seznam.cz>
2023-03-25 17:09:23 +0100

    Added inserts into tables

--- xnesut00_xstrei06.sql
+++ xnesut00_xstrei06.sql
@@ -11,7 +11,7 @@ DROP TABLE Bachelor_student CASCADE CONSTRAINTS;
 DROP TABLE Post_grad_studies CASCADE CONSTRAINTS;
 DROP TABLE Post_grad_student CASCADE CONSTRAINTS;
 
---create FUNCTION valid_birth_number(birth_number IN varchar(10), sex IN varchar(1) )   
+-- create FUNCTION valid_birth_number(birth_number IN varchar(10), sex IN varchar(1) )
 -- RETURN number  
 -- IS  
 --     valid number;  
@@ -33,16 +33,20 @@ DROP TABLE Post_grad_student CASCADE CONSTRAINTS;
 
 
 CREATE TABLE Person (
-    Birth_number VARCHAR(11) PRIMARY KEY,
+    Birth_number VARCHAR(10) PRIMARY KEY,
     Name_ VARCHAR(30) NOT NULL,
     Surname VARCHAR(30) NOT NULL,
     Sex VARCHAR(1) CHECK (Sex IN ('M', 'F')),
     Noble_title VARCHAR(30)
-    CHECK ((LENGTH(Birth_number) = 11) AND ISDATE())
+    -- CHECK ((LENGTH(Birth_number) = 10) AND ISDATE())
 );
 
 INSERT INTO Person
-VALUES ('020307/1234', 'Jaroslav', 'Streit', 'M', null);
+VALUES ('0203071231', 'Jaroslav', 'Streit', 'M', null);
+INSERT INTO Person
+VALUES ('0106122467', 'Matěj', 'Nesuta', 'M', 'Rytíř');
+INSERT INTO Person
+VALUES ('9755165673', 'Lenka', 'Zouharová', 'F', 'Hraběnka');
 
 CREATE TABLE Appointment (
     App_ID NUMBER(10) GENERATED AS IDENTITY (START WITH 1111 INCREMENT BY 1) PRIMARY KEY,
@@ -51,6 +55,8 @@ CREATE TABLE Appointment (
 
 INSERT INTO Appointment
 VALUES (DEFAULT, 'Jmenovan prezidentem');
+INSERT INTO Appointment
+VALUES (DEFAULT, 'Prijat ve vyberovem rizeni');
 
 CREATE TABLE Bureau_type (
     Type_ID NUMBER(10) GENERATED AS IDENTITY (START WITH 1111 INCREMENT BY 1) PRIMARY KEY,
@@ -59,14 +65,16 @@ CREATE TABLE Bureau_type (
 
 INSERT INTO Bureau_type
 VALUES (DEFAULT, 'Mestsky urad');
+INSERT INTO Bureau_type
+VALUES (DEFAULT, 'Krajsky urad');
 
 CREATE TABLE Bureau (
     Shortcut VARCHAR(10) PRIMARY KEY,
     Bureau_name VARCHAR(50) NOT NULL,
-    B_type VARCHAR(20) NOT NULL,
-    CONSTRAINT Bureau_ID FOREIGN KEY (B_type) REFERENCES Bureau_type (B_type),
+    B_type_ID NUMBER(10) NOT NULL,
+    CONSTRAINT Bureau_ID FOREIGN KEY (B_type_ID) REFERENCES Bureau_type (Type_ID),
     Founded DATE DEFAULT SYSDATE,
-    Closed DATE DEFAULT SYSDATE,
+    Closed DATE DEFAULT SYSDATE NULL,
     Deed_of_foundation NUMBER(10) NOT NULL UNIQUE,
     Publication_date DATE DEFAULT SYSDATE,
     City VARCHAR(50) NOT NULL,
@@ -74,24 +82,26 @@ CREATE TABLE Bureau (
     House_number NUMBER(10) NOT NULL,
     Postal_code NUMBER(5) NOT NULL CHECK(Postal_code > 9999),
     -- pro jednoduchost uvazujeme, ze oblastni pusobnost se vztahuje na oblast, kde se urad nachazi (podle adresy)
-    Area_of_operation VARCHAR(10) CHECK (Area_of_operation IN ('Království', 'Země', 'Region', 'Okres', 'Obec')),
+    Area_of_operation VARCHAR(10) CHECK (Area_of_operation IN ('Království', 'Země', 'Kraj', 'Okres', 'Obec')),
     Superior_shortcut VARCHAR(10) NULL,
     CONSTRAINT Superior_b FOREIGN KEY (Superior_shortcut) REFERENCES Bureau(Shortcut),
-    CHECK (Superior_shortcut <> Shortcut),
+    CONSTRAINT superior_shortcut_shortcut CHECK (Superior_shortcut <> Shortcut),
     CHECK (Founded < Closed)
 );
 
 INSERT INTO Bureau
-VALUES('MUBl', 'Mestsky urad Blansko', 'Mestsky urad', '11-JAN-2000', DEFAULT, 12345,
-       DEFAULT, 'Blansko', 'Hybesova', 69, 67801, 'Okres', NULL);
-
+VALUES('MUBl', 'Mestsky urad Blansko', 1111, '11-JAN-1998', NULL, 12345,
+       '29-JAN-1998', 'Blansko', 'Hybesova', 69, 67801, 'Okres', NULL);
 INSERT INTO Bureau
-VALUES('MUBo', 'Mestsky urad Boskovice', 'Mestsky urad', '11-JAN-2000', DEFAULT, 12346,
-       DEFAULT, 'Boskovice', 'Janackova', 55, 67801, 'Obec', NULL);
+VALUES('MUBo', 'Mestsky urad Boskovice', 1111, '09-SEP-2000', NULL, 12346,
+       DEFAULT, 'Boskovice', 'Janackova', 55, 67801, 'Obec', 'MUBl');
+INSERT INTO Bureau
+VALUES('KUPh', 'Krajsky urad Praha', 1112, '01-JAN-1993', NULL, 12347,
+       DEFAULT, 'Praha', 'Ulice Vaclava Havla', 1, 10000, 'Kraj', NULL);
 
 CREATE TABLE Function (
     Function_ID NUMBER(10) GENERATED AS IDENTITY (START WITH 1111 INCREMENT BY 1),
-    Function_name VARCHAR(30)  NOT NULL,
+    Function_name VARCHAR(50)  NOT NULL,
     Function_code NUMBER(10) NOT NULL,
     App_ID NUMBER(10),
     Function_length INTERVAL YEAR(4) TO MONTH DEFAULT INTERVAL '0000-00' YEAR TO MONTH,
@@ -103,35 +113,41 @@ CREATE TABLE Function (
     CONSTRAINT Superior_f FOREIGN KEY (Superior_ID) REFERENCES Function(Function_ID),
     CONSTRAINT Function_ID UNIQUE (Function_ID),
     CONSTRAINT Bureau FOREIGN KEY (F_bureau) REFERENCES Bureau(Shortcut),
-    constraint func_in_bur unique (Function_code, F_bureau)
+    CONSTRAINT func_in_bur UNIQUE (Function_code, F_bureau)
 );
 
 INSERT INTO Function
-VALUES(DEFAULT, 'Urednik', 1234, 1111, DEFAULT, null, DEFAULT, DEFAULT, 'MUBl');
-
+VALUES(DEFAULT, 'Urednik', 1234, 1111, DEFAULT, NULL, DEFAULT, DEFAULT, 'MUBl');
 INSERT INTO Function
-VALUES(DEFAULT, 'Urednik', 1234, 1111, DEFAULT, null, DEFAULT, DEFAULT, 'MUBo');
+VALUES(DEFAULT, 'Sekretarka', 1234, 1112, DEFAULT, 1111, DEFAULT, DEFAULT, 'MUBo');
+INSERT INTO Function
+VALUES(DEFAULT, 'Administrator systemu', 1357, 1111, DEFAULT, NULL, DEFAULT, DEFAULT, 'KUPh');
 
 CREATE TABLE Person_function (
     Table_ID NUMBER(10) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     Function_ID NUMBER(10),
     Date_from DATE DEFAULT SYSDATE NOT NULL,
-    Date_to DATE DEFAULT SYSDATE NOT NULL,
+    Date_to DATE DEFAULT SYSDATE,
     Employee_order NUMBER(5) NOT NULL,
     End_reason VARCHAR(150),
-    Birth_number VARCHAR(11),
+    Birth_number VARCHAR(10),
     CONSTRAINT VALID_INTERVAL CHECK (Date_to > Date_from),
     CONSTRAINT Person_ID FOREIGN KEY (Birth_number) REFERENCES Person (Birth_number),
     CONSTRAINT Func_ID FOREIGN KEY (Function_ID) REFERENCES Function (Function_ID)
 );
 
 INSERT INTO Person_function
-VALUES(DEFAULT, 1111, '07-MAR-2005', DEFAULT, 7, 'Nikdo nevi', '020307/1234');
+VALUES(DEFAULT, 1111, '07-MAR-2019', DEFAULT, 7, 'Nikdo nevi', '0203071231');
+INSERT INTO Person_function
+VALUES(DEFAULT, 1112, '11-MAR-2012', DEFAULT, 3,
+       'Nesplnovani pracovni doby, alkoholismus na pracovisti', '9755165673');
+INSERT INTO Person_function
+VALUES(DEFAULT, 1113, '21-JUN-2016', NULL, 2, NULL, '0106122467');
 
 CREATE TABLE Relationship (
     relationship_id NUMBER(10) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    Person_1 VARCHAR(11) NOT NULL,
-    Person_2 VARCHAR(11) NOT NULL,
+    Person_1 VARCHAR(10) NOT NULL,
+    Person_2 VARCHAR(10) NOT NULL,
     rel_type VARCHAR(20) CHECK (rel_type IN ('Family member', 'School', 'Work', 'Other')),
     rel_description VARCHAR(1000),
     CONSTRAINT Person_ID1 FOREIGN KEY (Person_1) REFERENCES Person (Birth_number),
@@ -140,17 +156,27 @@ CREATE TABLE Relationship (
     CHECK (person_1 <> person_2)
 );
 
+INSERT INTO Relationship
+VALUES(DEFAULT, '0203071231', '9755165673', 'Family member', 'Sestra');
+
 CREATE TABLE Decree (
     Decree_ID NUMBER(10) PRIMARY KEY,
     Date_of_decision DATE DEFAULT SYSDATE,
     Date_of_execution DATE DEFAULT SYSDATE,
     Decree VARCHAR(200) NOT NULL,
     Reasoning VARCHAR(1000) NOT NULL,
-    Person_ID VARCHAR(11),
+    Person_ID VARCHAR(10),
     Bureau_shortcut VARCHAR(10),
     CONSTRAINT D_person FOREIGN KEY (Person_ID) REFERENCES Person (Birth_number),
     CONSTRAINT D_bureau FOREIGN KEY (Bureau_shortcut) REFERENCES Bureau (Shortcut)
 );
+
+INSERT INTO Decree
+VALUES(731476, DEFAULT, DEFAULT, 'Vydani ridicskeho prukazu',
+       'Ridicsky prukaz vydan na zaklade plnohodnotneho splneni autoskoly.', '0203071231', 'MUBl');
+INSERT INTO Decree
+VALUES(731482, DEFAULT, DEFAULT, 'Vydani ridicskeho prukazu',
+       'Ridicsky prukaz vydan na zaklade plnohodnotneho splneni autoskoly.', '0203071231', 'MUBl');
 
 CREATE TABLE Bachelor_studies (
     Field_ID NUMBER(10) PRIMARY KEY,
@@ -159,24 +185,39 @@ CREATE TABLE Bachelor_studies (
     CONSTRAINT B_bureau FOREIGN KEY (S_bureau) REFERENCES  Bureau (Shortcut)
 );
 
+INSERT INTO Bachelor_studies
+VALUES(12, 'Manazerska Informatika', 'KUPh');
+INSERT INTO Bachelor_studies
+VALUES(16, 'Informacni technologie', 'KUPh');
+
 CREATE TABLE Post_grad_studies (
     Field_ID NUMBER(10) PRIMARY KEY,
     Name_ VARCHAR(50) NOT NULL,
-    Type_ VARCHAR(20) CHECK (Type_ IN ('Postgraduate certificates', 'Postgraduate diplomas', 'Master''s degrees', 'Doctorates')),
+    Type_ VARCHAR(25) CHECK (Type_ IN ('Postgraduate certificates', 'Postgraduate diplomas', 'Master''s degrees', 'Doctorates')),
     S_bureau VARCHAR(10),
     CONSTRAINT P_bureau FOREIGN KEY (S_bureau) REFERENCES  Bureau (Shortcut)
 );
 
+INSERT INTO Post_grad_studies
+VALUES(102, 'Umela inteligence', 'Postgraduate diplomas', 'KUPh');
+INSERT INTO Post_grad_studies
+VALUES(103, 'Kyberbezpecnost', 'Postgraduate diplomas', 'MUBl');
+
 CREATE TABLE Bachelor_student (
     Table_ID NUMBER(10) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     Date_from DATE DEFAULT SYSDATE,
-    Date_to DATE DEFAULT SYSDATE,
-    Successful_end VARCHAR(3) CHECK (Successful_end IN ('YES', 'NO')),
+    Date_to DATE DEFAULT SYSDATE NULL,
+    Successful_end VARCHAR(3) CHECK (Successful_end IN ('YES', 'NO')) NULL,
     S_studies NUMBER(10),
-    S_person VARCHAR(11),
+    S_person VARCHAR(10),
     CONSTRAINT Studies1 FOREIGN KEY (S_studies) REFERENCES  Bachelor_studies (Field_ID),
     CONSTRAINT Student1 FOREIGN KEY (S_person) REFERENCES  Person (Birth_number)
 );
+
+INSERT INTO Bachelor_student
+VALUES(DEFAULT, '21-SEP-2021', NULL, NULL, 16, '0203071231');
+INSERT INTO Bachelor_student
+VALUES(DEFAULT, '18-SEP-2018', '29-JUN-2022', 'YES', 12, '9755165673');
 
 CREATE TABLE Post_grad_student (
     Table_ID NUMBER(10) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -184,7 +225,10 @@ CREATE TABLE Post_grad_student (
     Date_to DATE DEFAULT SYSDATE,
     Successful_end VARCHAR(3) CHECK (Successful_end IN ('YES', 'NO')),
     S_studies NUMBER(10),
-    S_person VARCHAR(11),
+    S_person VARCHAR(10),
     CONSTRAINT Studies2 FOREIGN KEY (S_studies) REFERENCES  Post_grad_studies (Field_ID),
     CONSTRAINT Student2 FOREIGN KEY (S_person) REFERENCES  Person (Birth_number)
 );
+
+INSERT INTO Post_grad_student
+VALUES(DEFAULT, '23-SEP-2020', NULL, NULL, 103, '0106122467');
